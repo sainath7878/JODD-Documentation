3a0a046748a3fafaa27308a3bebb153f9358063f
Sainath <svmsainath@ymail.com>
2022-02-19 00:03:24 +0530

    Feat: Added Snackbar and lists

--- component/index.js
+++ component/index.js
@@ -1,4 +1,9 @@
 const sideNavAnchors = document.querySelectorAll(".side-nav-anchor");
+const snackbarShowBtn = document.querySelector(".snackbar-show-btn");
+const snackbarHideBtn = document.querySelector(".snackbar-hide-btn-demo");
+const snackbar = document.querySelector(".snackbar-demo");
+
+// -------- script for active side navbar link ------- 
 
 for(let i=0 ; i< sideNavAnchors.length;i++){
     sideNavAnchors[i].addEventListener("click",function()  {
@@ -7,3 +12,17 @@ for(let i=0 ; i< sideNavAnchors.length;i++){
         this.classList.toggle("active");
     })
 }
+// ----- script for snackbar --------- 
+
+
+const hideSnackbar = () => {
+    snackbar.style.display = "none";
+}
+
+const showSnackbar = () => {
+    snackbar.style.display= "flex";
+    setTimeout(hideSnackbar, 5000);
+}
+
+snackbarShowBtn.addEventListener("click", showSnackbar);
+snackbarHideBtn.addEventListener("click", hideSnackbar);
